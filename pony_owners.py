63cca441b23da48678798796d6d2591cda498e9c
miguelcoria94 <miguelcoria94@gmail.com>
2020-12-24 02:43:50 -0600

    updating file

--- pony_owners.py
+++ pony_owners.py
@@ -1,16 +1,68 @@
+# Put into a Python file in your project.
+# Activate your virtual environment.
+# Run with python file_name.py.
 from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy.schema import Column, ForeignKey, Table
+from sqlalchemy.types import Integer, String
+
+Base = declarative_base()
+
+pony_handlers = Table(
+    "pony_handlers",
+    Base.metadata,
+    Column("pony_id", ForeignKey("ponies.id"), primary_key=True),
+    Column("handler_id", ForeignKey("handlers.id"), primary_key=True))
+
+
+class Owner(Base):
+    __tablename__ = 'owners'
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(255))
+    last_name = Column(String(255))
+    email = Column(String(255))
+
+    ponies = relationship("Pony", back_populates="owner")
+
+
+class Pony(Base):
+    __tablename__ = "ponies"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    birth_year = Column(Integer)
+    breed = Column(String(255))
+    owner_id = Column(Integer, ForeignKey("owners.id"))
+
+    owner = relationship("Owner", back_populates="ponies")
+    handlers = relationship("Handler",
+                            secondary=pony_handlers,
+                            back_populates="ponies")
+
+
+class Handler(Base):
+    __tablename__ = "handlers"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    employee_id = Column(String(12))
+
+    ponies = relationship("Pony",
+                          secondary=pony_handlers,
+                          back_populates="handlers")
+
 
 db_url = "postgresql://sqlalchemy_test:password@localhost/sqlalchemy_test"
 engine = create_engine(db_url)
 
+SessionFactory = sessionmaker(bind=engine)
 
-with engine.connect() as connection:
-    result = connection.execute("""
-        SELECT o.first_name, o.last_name, p.name
-        FROM owners o
-        JOIN ponies p ON (o.id = p.owner_id)
-    """)
-    for row in result:
-        print(row["first_name"], row["last_name"], "owns", row["name"])
+session = SessionFactory()
 
+# Article code here.
+
+session.close()
 engine.dispose()
